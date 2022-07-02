5f609be430faa534b43433e49d2e53b3bd011309
sreejithliterally <imsreejithofficial@gmail.com>
2022-07-03 01:32:47 +0530

    add

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "./components/Header"
+
+export default function App(){
+    return(
+    <div>
+    
+        <Header />
+        <h1>Hello world</h1>
+        </div>
+    )
+        }
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Vector
+export default function Header(){
+    return ( 
+        <div>
+    <h3> Meme generaotr</h3>
+    <img src ={vector} alt="Vector"/>
+    </div>
+    )
+}
