6cd66dd1c0bc6334bf50eb50eb538a517e9974cd
sreejithliterally <imsreejithofficial@gmail.com>
2022-07-20 21:59:53 +0530

    new changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Body from "./components/Body"
 import Header from "./components/Header"
 
 export default function App(){
@@ -6,6 +7,7 @@ export default function App(){
     <div>
     
         <Header />
+        <Body />
        
         </div>
     )
---
+++ my-app/src/components/Body.js
@@ -0,0 +1,26 @@
+import React from "react"
+export default function Body(){
+return (
+    <div>
+        
+         <body className="body">
+             <div className="input">
+       <label> Enter Top text:   </label>
+       <input type="text"/>
+
+        <label> Enter Bottom text:   </label>
+        <input type="text"/>
+        </div>
+        
+        
+
+        <button class="submit-btn"
+        type="button">
+    Make Meme </button>
+
+
+        </body>
+
+    </div>
+)
+}
